ced6857c51fbde546c57a788f9b2cd3c00a5b58f
Matheus Gueff <gueffmatheus@gmail.com>
2025-06-05 22:10:01 -0300

    feat: adiciona criptografia para a senha do usuário

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,16 +1,23 @@
 import { UserModel } from "../models/user.model";
 import { jwtDecode } from 'jwt-decode';
 import dotenv from "dotenv";
-import { IUser } from "../interfaces/IUser.interface";
+import bcrypt from "bcryptjs"; 
+import { IUser } from "../interfaces/iUser.interface";
 dotenv.config();
 
 export const autenticar = async (email: string, password: string) => {
   const login = await UserModel.findOne({
     where: {
-      email: email,
-      senha: password,
+      email: email
     },
   });
+
+  if(!login) return null;
+
+  const isMatch = await bcrypt.compare(password, login.senha)
+  
+  if(!isMatch) return null;
+   
   return login;
 };
 
