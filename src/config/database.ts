e6c89aefe425183aeba811ce77b4da0b2d874c77
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-06-20 18:48:07 -0300

    fix: revertendo alterações para correção de erros na hospedagem

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,21 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+const env = process.env.NODE_ENV || "development"; 
+
+const databaseUrl = env == 'development' 
+  ? process.env.DEV_DATABASE_URL 
+  : process.env.PROD_DATABASE_URL
+
+if (env === 'production' && !process.env.PROD_DATABASE_URL) {
+  throw new Error('PROD_DATABASE_URL é necessário para produção');
+}
+
+if (env === 'development' && !process.env.DEV_DATABASE_URL) {
+  throw new Error('DEV_DATABASE_URL é necessário para desenvolvimento');
+}
+
+const sequelize = new Sequelize(databaseUrl!, {
   dialect: "postgres",
   protocol: "postgres",
   dialectOptions: {
