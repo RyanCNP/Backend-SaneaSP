843f62649de03ffc2647582d441f5372d367f84c
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-07-14 10:01:48 -0300

    refactor: rota de autenticação corrigida com o mesmo padrão de tratamento de erros realizada na rota de categorias

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import { autenticar} from "../controllers/auth.controller";
 import { ApiError } from "../errors/ApiError.error";
@@ -7,43 +7,20 @@ dotenv.config();
 
 export const authRoutes = express.Router();
 
-authRoutes.post("/", async (req: Request, res: Response) => {
+authRoutes.post("/", async (req: Request, res: Response, next : NextFunction) => {
   try {
     const { email, senha } = req.body;
     const token = await autenticar(email, senha);
     res.status(200).json(token);
   } catch (error) {
-    //Verifica se o erro é uma ApiError
-    if(error instanceof ApiError){
-      res.status(error.httpStatus).json({
-        message: error.message,
-        error: true,
-      });
-    }
-    else{
-      res.status(500).json({
-        message: "Ocorreu um erro de servidor: " + error,
-        error: true,
-      });
-    }
+   next(error);
   }
 });
 
-authRoutes.get("/me", validateToken, async (req: Request, res: Response) => {
+authRoutes.get("/me", validateToken, async (req: Request, res: Response, next : NextFunction) => {
   try {
     res.status(200).json(req.user)
   } catch (error) {
-    if(error instanceof ApiError){
-      res.status(error.httpStatus).json({
-        message : error.message,
-        error: true
-      })
-    }
-    else{
-      res.status(500).json({
-        message : 'Ocorreu um erro de servidor' + error,
-        error: true
-      })
-    }
+    next(error);
   }
 });
