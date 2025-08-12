e0bd7922dd139faf3c0b12c2a643703d0e269ba8
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-08-12 13:16:21 -0300

    chore: altera conexão com banco de dados para utilizar o banco local durante desenvolvimento

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,30 +2,36 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const env = process.env.NODE_ENV || "development"; 
+const env = process.env.NODE_ENV || "development";
 
-const databaseUrl = env == 'development' 
-  ? process.env.DEV_DATABASE_URL 
-  : process.env.PROD_DATABASE_URL
+let sequelize: Sequelize;
 
-if (env === 'production' && !process.env.PROD_DATABASE_URL) {
-  throw new Error('PROD_DATABASE_URL é necessário para ambiente de produção');
-}
+if (env === "development") {
+  sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./database.sqlite",
+    logging: false,
+  });
+} 
+else if (env === "production") {
+  const databaseUrl = process.env.PROD_DATABASE_URL;
 
-if (env === 'development' && !process.env.DEV_DATABASE_URL) {
-  throw new Error('DEV_DATABASE_URL é necessário para ambiente de desenvolvimento');
-}
+  if (!databaseUrl) {
+    throw new Error("PROD_DATABASE_URL é necessário para ambiente de produção");
+  }
 
-const sequelize = new Sequelize(databaseUrl!, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+  sequelize = new Sequelize(databaseUrl, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-  logging: false,
-});
+    logging: false,
+  });
+} 
+else throw new Error(`Ambiente desconhecido: ${env}`);
 
 export default sequelize;
