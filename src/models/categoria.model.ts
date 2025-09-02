90852bc34b4ff8c9b360c231a8e4cdc0d97c51e3
Ryan <ryanjobs234@gmail.com>
2025-09-02 20:57:49 -0300

    fix(database): correção do nome do arquivo juntamente com as importações do mesmo

--- src/models/categoria.model.ts
+++ src/models/categoria.model.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model, Optional } from "sequelize";
-import sequelize from '../config/database'
+import sequelize from '../config/database.config'
 import { ICategoria } from "../interfaces/categoria";
 
 type CategoriaCreationalAttributes = Optional<ICategoria, "id">
