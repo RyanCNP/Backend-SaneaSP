8b79a051d59db882c6e48965df87832fc01dad9c
Ryan <ryanjobs234@gmail.com>
2025-09-03 21:31:01 -0300

    feat(user): adição do campo de verificação do tipo boolean

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -14,6 +14,7 @@ export class UserModel extends Model<IUser, IUserCreationAttributes> {
     public cpf!: string;
     public endereco?: IEndereco;
     public nivel!: UserLevel;
+    public verified!: boolean;
 }
 
 UserModel.init({
@@ -61,6 +62,12 @@ UserModel.init({
         allowNull: false,
         defaultValue: UserLevel.COMMON,
         field: 'nivel'
+    },
+    verified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+        field: 'verified'
     }
 }, {
     tableName: 'usuario',
