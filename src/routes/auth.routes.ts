7555b1945f9d8d9fdebeec567022e6713bd7eb00
Ryan <ryanjobs234@gmail.com>
2025-09-05 19:33:09 -0300

    fix(email): remoção do try e catch dos métodos de criação de usuário e verificação do token de confirmação de cadastro na rota de criação e verificação

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
-import express, {Request, Response } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { autenticar, registerUser} from "../controllers/auth.controller";
+import { autenticar, registerUser } from "../controllers/auth.controller";
 import { validateToken } from "../middlewares/auth.middleware";
 import jwt from "jsonwebtoken";
 import { UserModel } from "../models/user.model";
@@ -17,41 +17,26 @@ authRoutes.post("/login", async (req: Request, res: Response) => {
 
 //Cadastro
 authRoutes.post("/register", async (req: Request, res: Response) => {
-  try {
-    const data = await registerUser(req.body);
+  const data = await registerUser(req.body);
 
-    res.status(201).json({
-      error: false,
-      message: "Cadastro realizado! Verifique seu e-mail para ativar sua conta.",
-      data,
-    });
-  } catch (error: any) {
-    console.error(error);
-    res.status(500).json({
-      error: true,
-      message: error.message || "Erro ao registrar usuário.",
-    });
-  }
+  res.status(201).json({
+    error: false,
+    message: "Cadastro realizado! Verifique seu e-mail para ativar sua conta.",
+    data,
+  });
 });
 
 // Rota de confirmação de cadastro
 authRoutes.get("/confirm/:token", async (req: Request, res: Response) => {
   const { token } = req.params;
+  const secretKey = process.env.SECRET_KEY || "";
+  const decoded: any = jwt.verify(token, secretKey);
 
-  try {
-    const secretKey = process.env.SECRET_KEY || "";
-    const decoded: any = jwt.verify(token, secretKey);
-
-    await UserModel.update(
-      { verified: true },
-      { where: { id: decoded.id } }
-    );
-
-    res.json({ message: "Conta verificada com sucesso!" });
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ message: "Token inválido ou expirado." });
-  }
+  await UserModel.update(
+    { verified: true },
+    { where: { id: decoded.id } }
+  );
+  res.json({ message: "Conta verificada com sucesso!" });
 });
 
 //Dados do usuário logado
