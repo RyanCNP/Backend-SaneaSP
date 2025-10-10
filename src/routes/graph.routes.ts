8e42cc48907807c457399fe6f56d13d4d6507e1b
MathGueff <gueffmatheus@gmail.com>
2025-10-10 20:19:24 -0300

    refactor(graph): refatora o feature gráfico para o padrão

--- src/routes/graph.routes.ts
+++ src/routes/graph.routes.ts
@@ -1,13 +1,8 @@
-import  express, { Request, Response,  }  from "express";
-import { IFilterListDenuncia } from "../interfaces/denuncia";
-import { BigPoints, IfilterGraph } from "../interfaces/graph";
-import { getBigPoints } from "../controllers/graph.controller";
-export const router = express.Router();
+import express from "express"
+import { getMaioresPontuacoes } from "../controllers/graph.controller"
 
-router.get('/maioresPontuacoes',async(req:Request,res:Response)=>{
-    const filter = req.query as unknown as IfilterGraph;
-    const result:BigPoints[] = await getBigPoints(filter)
-    res.json(result);
-});
+export const router = express.Router()
 
-export default router;
+router.get("/maioresPontuacoes", getMaioresPontuacoes)
+
+export default router
