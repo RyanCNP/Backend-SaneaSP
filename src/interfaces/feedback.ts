6209c8befd3a8ac3a2ca49357d06716b436dbb3a
Ryan <ryanjobs234@gmail.com>
2025-10-14 12:26:04 -0300

    feat(feedback): criação do ICreateFeedback

--- src/interfaces/feedback.ts
+++ src/interfaces/feedback.ts
@@ -6,3 +6,11 @@ export interface IFeedback {
     fk_denuncia: number,
     fk_cidadao: number
 }
+
+export interface ICreateFeedback {
+    data_publicacao: Date,
+    descricao: string,
+    fk_funcionario: number,
+    fk_denuncia: number,
+    fk_cidadao: number
+}
