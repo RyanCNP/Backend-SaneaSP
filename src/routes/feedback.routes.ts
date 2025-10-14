4a9beb5ebed771eb383cf11c5bf7758d484b8394
Ryan <ryanjobs234@gmail.com>
2025-10-14 12:33:32 -0300

    feat(feedback): rotas

---
+++ src/routes/feedback.routes.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import * as feedbackController from "../controllers/feedback.controller"
+import { validateToken } from "../middlewares/auth.middleware"
+
+const router = express.Router()
+
+router.get("/", feedbackController.getAllFeedbacks)
+router.get("/:id", feedbackController.getById)
+
+router.use(validateToken)
+
+router.get("/feedback-cidadao/:id", feedbackController.getByCidadao)
+router.get("/feedback-denuncia/:id", feedbackController.getByDenuncia)
+
+router.post("/create-feedback", feedbackController.postFeedback)
+router.put("/:id", feedbackController.putFeedback)
+router.delete("/:id", feedbackController.deleteFeedback)
+
+export default router
