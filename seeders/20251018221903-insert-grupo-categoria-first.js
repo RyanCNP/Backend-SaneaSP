a274dda7731f53254f52f63f81248268233789f1
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-10-18 19:51:34 -0300

    feat: adiciona try catch com log e rollback para seeders

--- seeders/20251018221903-insert-grupo-categoria-first.js
+++ seeders/20251018221903-insert-grupo-categoria-first.js
@@ -3,28 +3,36 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('grupo_categoria', [
-      {
-        id: 1,
-        nome : "Água",
-        icone: "water.webp"
-      },
-      {
-        id: 2,
-        nome : "Drenagem",
-        icone: "drainage.webp"
-      },
-      {
-        id: 3,
-        nome : "Esgoto",
-        icone: "sewage.webp"
-      },
-      {
-        id: 4,
-        nome : "Limpeza",
-        icone: "cleaning.webp"
-      }
-    ])
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.bulkInsert('grupo_categoria', [
+        {
+          id: 1,
+          nome : "Água",
+          icone: "water.webp"
+        },
+        {
+          id: 2,
+          nome : "Drenagem",
+          icone: "drainage.webp"
+        },
+        {
+          id: 3,
+          nome : "Esgoto",
+          icone: "sewage.webp"
+        },
+        {
+          id: 4,
+          nome : "Limpeza",
+          icone: "cleaning.webp"
+        }
+      ], transaction)
+      await transaction.commit();
+    } catch (error) {
+       await transaction.rollback();
+      console.log('Erro na seed INSERT-GRUPO-CATEGORIA - rollback realizado:', error.errors || error.message);
+      throw error;
+    }
   },
 
   async down (queryInterface, Sequelize) {
