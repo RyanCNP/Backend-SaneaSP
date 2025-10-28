2cc79e435f8c1a70ebddbdeb6f881ec610e309c4
Davy Ribeiro <davy.oliv.ribeiro@gmail.com>
2025-10-28 20:21:07 -0300

    feat!(registro): migration de registro

---
+++ migrations/20251028230755-create-table-registro.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("registro", {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      descricao: {
+        allowNull: false,
+        type: DataTypes.STRING(500),
+      },
+      data_publicacao: {
+        allowNull: false,
+        type: DataTypes.DATE(),
+      },
+      tipo: {
+        allowNull: false,
+        type: DataTypes.STRING(250),
+      },
+      arquivo:{
+        allowNull:true,
+        type:DataTypes.STRING(250)
+      },
+      id_denuncia: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "denuncia",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      id_usuario:{
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "usuario",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+
+    await queryInterface.dropTable('registro');
+
+  },
+};
