fa35c47f4f164db979abb6340603c75fac4120e4
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-11-02 19:51:38 -0300

    feat: adiciona relacionamento usuario x cidadao

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,43 +5,81 @@ import { ImagemDenunciaModel } from "./imagem-denuncia.model";
 import { ComentarioModel } from "./comentario.model";
 import { UserModel } from "./user.model";
 import { GrupoCategoriaModel } from "./grupo-categoria.model";
+import { CidadaoModel } from "./cidadao.model";
 
-//Adicionando associação entre Tag e Reclamações
-DenunciaModel.belongsToMany(CategoriaModel,
-    { through: CategoriaDenunciaModel, foreignKey: 'id_denuncia', as: 'categorias' })
-CategoriaModel.belongsToMany(DenunciaModel,
-    { through: CategoriaDenunciaModel, foreignKey: 'id_categoria', as: 'denuncias' })
+//CATEGORIAS E DENUNCIAS
+DenunciaModel.belongsToMany(CategoriaModel, {
+  through: CategoriaDenunciaModel,
+  foreignKey: "id_denuncia",
+  as: "categorias",
+});
+CategoriaModel.belongsToMany(DenunciaModel, {
+  through: CategoriaDenunciaModel,
+  foreignKey: "id_categoria",
+  as: "denuncias",
+});
 
-DenunciaModel.belongsToMany(CategoriaModel,
-    { through: CategoriaDenunciaModel, foreignKey: 'id_denuncia', as: "categoriasSelecionadas" })
+DenunciaModel.belongsToMany(CategoriaModel, {
+  through: CategoriaDenunciaModel,
+  foreignKey: "id_denuncia",
+  as: "categoriasSelecionadas",
+});
 
-//Adicionando associação entre Imagem e Reclamações
-DenunciaModel.hasMany(ImagemDenunciaModel,
-    { foreignKey: 'id_denuncia', as: 'imagens', onDelete: 'CASCADE' })
-ImagemDenunciaModel.belongsTo(DenunciaModel,
-    { foreignKey: 'id_denuncia', as: 'denuncia' })
+//IMAGENS_DENUNCIA E DENUNCIAS
+DenunciaModel.hasMany(ImagemDenunciaModel, {
+  foreignKey: "id_denuncia",
+  as: "imagens",
+  onDelete: "CASCADE",
+});
+ImagemDenunciaModel.belongsTo(DenunciaModel, {
+  foreignKey: "id_denuncia",
+  as: "denuncia",
+});
 
-ComentarioModel.belongsTo(UserModel, 
-    { foreignKey: 'fkUsuario', as: 'usuario'});
+//COMENTARIOS E DENUNCIAS
+ComentarioModel.belongsTo(UserModel, {
+  foreignKey: "fkUsuario",
+  as: "usuario",
+});
 
-ComentarioModel.belongsTo(DenunciaModel,
-    {foreignKey:"fkDenuncia",as:'denuncia'}
-    
-)
-//Adicionando associação entre GrupoCategoria e Categorias
-GrupoCategoriaModel.hasMany(CategoriaModel,
-    {foreignKey: 'id_grupo', as : 'categorias', onDelete: 'CASCADE'}
-)
-CategoriaModel.belongsTo(GrupoCategoriaModel,
-    {foreignKey: 'id_grupo', as : 'grupo', onDelete: 'CASCADE'}
-)
+ComentarioModel.belongsTo(DenunciaModel, {
+  foreignKey: "fkDenuncia",
+  as: "denuncia",
+});
+
+//GRUPO_CATEGORIAS E CATEGORIAS
+GrupoCategoriaModel.hasMany(CategoriaModel, {
+  foreignKey: "id_grupo",
+  as: "categorias",
+  onDelete: "CASCADE",
+});
+CategoriaModel.belongsTo(GrupoCategoriaModel, {
+  foreignKey: "id_grupo",
+  as: "grupo",
+  onDelete: "CASCADE",
+});
+
+//USUARIO E CIDADÃO/PREFEITURA/FUNCIONARIO
+UserModel.hasOne(CidadaoModel, {
+  foreignKey: "idUsuario",
+  as: "cidadao",
+  onDelete: "CASCADE",
+});
+
+CidadaoModel.belongsTo(UserModel, { 
+    foreignKey: "idUsuario", 
+    as: "usuario",
+    onDelete: "CASCADE"
+});
 
 //Fazendo o export dos models com as modificações
 export {
-    DenunciaModel,
-    CategoriaModel,
-    CategoriaDenunciaModel,
-    ImagemDenunciaModel,
-    ComentarioModel,
-    GrupoCategoriaModel
+  DenunciaModel,
+  CategoriaModel,
+  CategoriaDenunciaModel,
+  ImagemDenunciaModel,
+  ComentarioModel,
+  GrupoCategoriaModel,
+  UserModel,
+  CidadaoModel
 };
