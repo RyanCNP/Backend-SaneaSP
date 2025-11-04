ee3c6c5aedf534ce557a4e31c0f2eb991158d3df
Pedro8k <161396616+Pedro8k@users.noreply.github.com>
2025-11-03 21:58:57 -0300

    feat: adiciona visita

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,14 +1,12 @@
+
 import { CategoriaModel } from "./categoria.model";
 import { DenunciaModel } from "./denuncia.model";
 import { CategoriaDenunciaModel } from "./categoria-denuncia.model";
 import { ImagemDenunciaModel } from "./imagem-denuncia.model";
-import { ComentarioModel } from "./comentario.model";
-import { UserModel } from "./user.model";
-import { GrupoCategoriaModel } from "./grupo-categoria.model";
-import { RegistroModel } from "./registro.model";
-import { ImagemRegistroModel } from "./imagem-registro.model";
+import { Registro } from "./registro.model"; // I
+import { Visita } from "./visita.model"; 
 
-//Adicionando associação entre Tag e Reclamações
+
 DenunciaModel.belongsToMany(CategoriaModel,
     { through: CategoriaDenunciaModel, foreignKey: 'id_denuncia', as: 'categorias' })
 CategoriaModel.belongsToMany(DenunciaModel,
@@ -17,43 +15,30 @@ CategoriaModel.belongsToMany(DenunciaModel,
 DenunciaModel.belongsToMany(CategoriaModel,
     { through: CategoriaDenunciaModel, foreignKey: 'id_denuncia', as: "categoriasSelecionadas" })
 
-//Adicionando associação entre Imagem e Reclamações
+
 DenunciaModel.hasMany(ImagemDenunciaModel,
     { foreignKey: 'id_denuncia', as: 'imagens', onDelete: 'CASCADE' })
 ImagemDenunciaModel.belongsTo(DenunciaModel,
     { foreignKey: 'id_denuncia', as: 'denuncia' })
 
-ComentarioModel.belongsTo(UserModel, 
-    { foreignKey: 'fkUsuario', as: 'usuario'});
 
-ComentarioModel.belongsTo(DenunciaModel,
-    {foreignKey:"fkDenuncia",as:'denuncia'}
-    
-)
-//Adicionando associação entre GrupoCategoria e Categorias
-GrupoCategoriaModel.hasMany(CategoriaModel,
-    {foreignKey: 'id_grupo', as : 'categorias', onDelete: 'CASCADE'}
-)
-CategoriaModel.belongsTo(GrupoCategoriaModel,
-    {foreignKey: 'id_grupo', as : 'grupo', onDelete: 'CASCADE'}
-)
+Registro.hasMany(Visita, {
+    foreignKey: 'fk_registro',
+    as: 'visitas',
+    onDelete: 'CASCADE'
+});
 
-RegistroModel.belongsTo(DenunciaModel,{foreignKey:'id_denuncia',as:'denuncia'})
-RegistroModel.belongsTo(UserModel,{foreignKey:'id_usuario',as:'usuario'})
+Visita.belongsTo(Registro, {
+    foreignKey: 'fk_registro',
+    as: 'registro'
+});
 
-RegistroModel.hasMany(ImagemRegistroModel,
-    { foreignKey: 'id_registro', as: 'arquivos', onDelete: 'CASCADE' })
-ImagemRegistroModel.belongsTo(RegistroModel,
-    { foreignKey: 'id_registro', as: 'registro' })
 
-//Fazendo o export dos models com as modificações
 export {
     DenunciaModel,
     CategoriaModel,
     CategoriaDenunciaModel,
     ImagemDenunciaModel,
-    ComentarioModel,
-    GrupoCategoriaModel,
-    RegistroModel,
-    ImagemRegistroModel
+    Registro,
+    Visita
 };
