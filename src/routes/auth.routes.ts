e2cb1967711ea4c4491b15947255ce4dc7e98ac9
Ryan <ryanjobs234@gmail.com>
2025-11-08 15:59:05 -0300

    feat: adição do lostPassword no auth.routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { autenticar, cadastroCidadao, cadastroFuncionario, emailConfirmation, getAuthenticatedUser, testeCadastroUsuarioComum } from "../controllers/auth.controller";
+import { autenticar, cadastroCidadao, cadastroFuncionario, emailConfirmation, getAuthenticatedUser, testeCadastroUsuarioComum, lostPassword } from "../controllers/auth.controller";
 import { validateToken } from "../middlewares/auth.middleware";
 import { userCreateMid } from "../middlewares/user-create.middleware";
 import { userAlreadyExists } from "../middlewares/user-already-exists.middleware";
@@ -19,8 +19,9 @@ authRoutes.post("/register/cidadao", withTransaction, userAlreadyExists, userCre
 authRoutes.post("/register/funcionario", withTransaction, userAlreadyExists, userCreateMid(UserType.FUNCIONARIO), cadastroFuncionario);
 // authRoutes.post("/register/prefeitura", withTransaction, userExistsMid, userCreateMid(UserType.PREFEITURA), cadastroPrefeitura);
 
-// Rota de confirmação de cadastro
+// Rota de confirmação e troca de senha
 authRoutes.get("/confirm/:token", emailConfirmation);
+authRoutes.post("/lost-password", lostPassword);
 
 //Dados do usuário logado
 authRoutes.get("/me", validateToken, getAuthenticatedUser);
